1463e5bea38dbb307418b692cad1f7169f050049
LucaCrotti <crottilc@gmail.com>
2021-05-13 10:03:33 +0200

    Parameter estimator e service

--- src/Direct_kinematics/src/Kinematics.cpp
+++ src/Direct_kinematics/src/Kinematics.cpp
@@ -9,7 +9,8 @@
 #include <agilex/parametersConfig.h>
 #include <tf/transform_broadcaster.h>
 #include "robotics_hw1/CustomOdom.h"
-
+#include "agilex/set_pose.h"
+#include "agilex/reset_pose.h"
 
 class pub_sub {
 
@@ -24,6 +25,8 @@ public:
   int count=0;
   tf::TransformBroadcaster br; 
   robotics_hw1::CustomOdom custom_msg;
+  ros::ServiceServer service;
+  ros::ServiceServer service1;
 
 private:
   ros::NodeHandle n; 
@@ -44,6 +47,8 @@ public:
     sub = n.subscribe("velocity_bot", 1, &pub_sub::callback_m1, this);
     pub = n.advertise<nav_msgs::Odometry>("/agilex_odom", 1);
     pub2 = n.advertise<robotics_hw1::CustomOdom>("/custom_odom", 1);
+    service = n.advertiseService("set_pose", &pub_sub::set, this);
+    service1 = n.advertiseService("reset_pose", &pub_sub::reset, this);
 
 
     f = boost::bind(&pub_sub::callback_param, this, _1, _2);
@@ -64,6 +69,7 @@ public:
     speed=*msg;
 
     tf::Quaternion q;
+    tf::Quaternion q1;
     //     msg->pose.pose.orientation.x,
     //     msg->pose.pose.orientation.y,
     //     msg->pose.pose.orientation.z,
@@ -97,21 +103,27 @@ public:
        position_now_odom.pose.pose.position.x=position_now.x;
     position_now_odom.pose.pose.position.y=position_now.y;
     q.setRPY(0,0,position_now.theta);
-    //q.normalize();
+    q1.setRPY(0,0,0);
+    q1.normalize();
     position_now_odom.pose.pose.orientation.x=q[0];
     position_now_odom.pose.pose.orientation.y=q[1];
     position_now_odom.pose.pose.orientation.z=q[2];
     position_now_odom.pose.pose.orientation.w=q[3];
-    position_now_odom.header.stamp=ros::Time::now();
+    position_now_odom.header.stamp=speed.header.stamp;
     position_now_odom.child_frame_id="robottino";
     position_now_odom.header.frame_id="map";
 
     pub.publish(position_now_odom);
 
     tf::Transform transform;
+    tf::Transform transform1;
+
     transform.setOrigin( tf::Vector3(position_now.x, position_now.y, 0) );
     transform.setRotation(q);
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "agilex"));
+    transform1.setRotation(q1);
+    br.sendTransform(tf::StampedTransform(transform, speed.header.stamp, "map", "agilex"));
+    br.sendTransform(tf::StampedTransform(transform1,speed.header.stamp, "map", "odom"));
+    br.sendTransform(tf::StampedTransform(transform1,speed.header.stamp, "map", "world"));
     custom_msg.odom= position_now_odom;
     pub2.publish(custom_msg);
 
@@ -129,6 +141,31 @@ public:
 
     ROS_INFO("Parameter received %d", integration_type);
   }
+
+
+  bool set(agilex::set_pose::Request  &req, agilex::set_pose::Response &res)
+  {
+  position_old.x = req.x;
+  position_old.y = req.y;
+  position_old.theta = req.theta;
+
+  res.info="Position is set";
+
+  ROS_INFO("New pose is set");
+  return true;
+  }
+
+  bool reset(agilex::reset_pose::Request  &req, agilex::reset_pose::Response &res)
+  {
+  position_old.x = 0;
+  position_old.y = 0;
+  position_old.theta = 0;
+
+  res.info="Position is reset";
+  ROS_INFO("New pose is reset");
+  return true;
+  }
+
 };
 
 
--- src/parameter_estimator/src/sub_pol.cpp
+++ src/parameter_estimator/src/sub_pol.cpp
@@ -1,6 +1,7 @@
 
 #include "ros/ros.h"
 #include "geometry_msgs/Vector3Stamped.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "robotics_hw1/MotorSpeed.h"
 #include "robotics_hw1/DiffDriveSpeed.h"
 #include "nav_msgs/Odometry.h"
@@ -8,15 +9,62 @@
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
-
+#include <tf/tf.h>
+#include <math.h>
+namespace old 
+{
+double theta_old;
+double x_old;
+double y_old;
+double time_old=4;
+ }
 
 void callback(const robotics_hw1::MotorSpeedConstPtr& msg1, 
               const robotics_hw1::MotorSpeedConstPtr& msg2,
               const robotics_hw1::MotorSpeedConstPtr& msg3,
               const robotics_hw1::MotorSpeedConstPtr& msg4,
-              const nav_msgs::OdometryConstPtr& msg5) {
+              /*const nav_msgs::OdometryConstPtr& msg5,*/
+              const geometry_msgs::PoseStampedConstPtr& msg6) {
    const double est_gear= 38.20 ;
+  
+   double dt=msg6->header.stamp.toSec()-old::time_old;
+   double vel,omega;
 
+   double x_now=msg6->pose.position.x;
+   double y_now=msg6->pose.position.y;
+
+   double v_x= (x_now-old::x_old)/dt;
+   double v_y= (y_now-old::y_old)/dt;
+
+   old::x_old=x_now;
+   old::y_old=y_now;
+
+   vel=sqrt(v_x*v_x+v_y*v_y);
+   double theta2=atan2(v_y,v_x);
+
+   tf::Quaternion q;
+
+   q[0]= msg6->pose.orientation.x;
+   q[1]= msg6->pose.orientation.y;
+   q[2]= msg6->pose.orientation.z;
+   q[3]= msg6->pose.orientation.w;
+
+   double roll, pitch , theta;
+  tf::Matrix3x3(q).getRPY(roll, pitch, theta);
+
+  theta=(theta+4);
+
+  double d_theta=(theta)-old::theta_old;
+
+  
+  old::theta_old=theta;
+  //ROS_INFO("Tempo %f ",old::time_old);
+  old::time_old=msg6->header.stamp.toSec();
+
+  omega=d_theta/dt;
+ //ROS_INFO("Tempo %f ",theta+4);
+
+ /*  
    ROS_INFO ("Received 4 messages: (%f,%f,%f)", 
      ((msg1->rpm)+(msg3->rpm))*0.000434/2,((msg2->rpm)+(msg4->rpm))*0.000434/2, msg5->twist.twist.angular.z);
    if (((((msg1->rpm)+(msg3->rpm))/2)<0&&(((msg2->rpm)+(msg4->rpm))/2)<0)||((((msg1->rpm)+(msg3->rpm))/2)>0&&(((msg2->rpm)+(msg4->rpm))/2)>0))
@@ -30,8 +78,25 @@ void callback(const robotics_hw1::MotorSpeedConstPtr& msg1,
      double gear= ((-msg1->rpm+msg2->rpm-msg3->rpm+msg4->rpm)*3.14*0.1575)/(4*30*msg5->twist.twist.linear.x);
      ROS_INFO ("Estimated gear: (%f)",gear);
    }
+   */
+  if (dt<0.04 && abs(omega)>0.001)
+{
+   if (((((msg1->rpm)+(msg3->rpm))/2)<0&&(((msg2->rpm)+(msg4->rpm))/2)<0)||((((msg1->rpm)+(msg3->rpm))/2)>0&&(((msg2->rpm)+(msg4->rpm))/2)>0))
+   {
+     double y0=(((((msg1->rpm)+(msg3->rpm))/2)+(((msg2->rpm)+(msg4->rpm))/2))*2*3.14*0.1575)/(est_gear*60*omega);
+
+    ROS_INFO ("Received 4 messages: (%f,%f,%f,%f,%f)", 
+     ((msg1->rpm)+(msg3->rpm))*0.000434/2,((msg2->rpm)+(msg4->rpm))*0.000434/2,dt ,omega, d_theta);
+     ROS_INFO ("Estimated Y0: (%f)",y0);
+   }
+ }
+
+  /*if (((((msg1->rpm)+(msg3->rpm))/2)<0&&(((msg2->rpm)+(msg4->rpm))/2)>0)||((((msg1->rpm)+(msg3->rpm))/2)>0&&(((msg2->rpm)+(msg4->rpm))/2)<0))
+   {
+     double gear= ((-msg1->rpm+msg2->rpm-msg3->rpm+msg4->rpm)*3.14*0.1575)/(4*30*vel);
+     ROS_INFO ("Estimated gear: (%f)",gear);
+   }*/
   
- 
   //pub1.publish(speed);
 }
 
@@ -45,15 +110,15 @@ int main(int argc, char** argv) {
   message_filters::Subscriber<robotics_hw1::MotorSpeed> sub2(n, "motor_speed_fr", 1);
   message_filters::Subscriber<robotics_hw1::MotorSpeed> sub3(n, "motor_speed_rl", 1);
   message_filters::Subscriber<robotics_hw1::MotorSpeed> sub4(n, "motor_speed_rr", 1);
-  message_filters::Subscriber<nav_msgs::Odometry> sub5(n, "scout_odom", 1);
-   
+  /*message_filters::Subscriber<nav_msgs::Odometry> sub5(n, "scout_odom", 1);*/
+  message_filters::Subscriber<geometry_msgs::PoseStamped> sub6(n, "gt_pose", 1);
 
   //typedef message_filters::sync_policies::ExactTime<geometry_msgs::Vector3Stamped, geometry_msgs::Vector3Stamped> MySyncPolicy;
   typedef message_filters::sync_policies
-      ::ApproximateTime<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, nav_msgs::Odometry> MySyncPolicy;
+      ::ApproximateTime<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, /*nav_msgs::Odometry, */ geometry_msgs::PoseStamped> MySyncPolicy;
   
-  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub1, sub2, sub3, sub4, sub5);
-  sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4, _5));
+  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub1, sub2, sub3, sub4, /*sub5,*/ sub6);
+  sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4, _5/*, _6*/));
 
   ros::spin();
 
